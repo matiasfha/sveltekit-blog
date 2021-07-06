4c30575306d73a4845deb4979475ed01069da7e9
Matias Hernandez <matiasfh@gmail.com>
2021-07-05 23:01:34 -0400

    setup codesandbox

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
+import staticAdapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +14,8 @@ const config = {
 	})],
 
 	kit: {
-		adapter: vercel(),
+		//adapter: vercel(),
+		adapter: staticAdapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
